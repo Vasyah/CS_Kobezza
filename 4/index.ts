960c0985a19f4346bc633ab011b1c2833214c758
Vasyah <Souji_933@mail.ru>
2024-03-24 11:06:17 +0300

    Добавил typescript

---
+++ 4/index.ts
@@ -0,0 +1,35 @@
+class BCD {
+    numbers: number[] = [];
+
+    constructor(num: bigint) {
+        /* конвертировать исходное число в BCD
+       если число больше, чем 28 бит (7 bcd символов), то
+       оставшуюся часть добавляем следующем элементом массива this.numbers
+    */
+
+        const symbols = num.toString().split("");
+        const BCD_SYMBOL_SIZE = 4;
+        const MAX_DIGIT_SIZE = 8;
+
+        symbols.forEach((symbol, index) => {
+            const idx = Math.floor(index / MAX_DIGIT_SIZE);
+            this.numbers[idx] = (this.numbers[idx] << BCD_SYMBOL_SIZE) | +symbol;
+        });
+    }
+
+    // метод возвращает
+    valueOf() {
+        return this.numbers;
+    }
+
+    get(idx) {}
+}
+
+const n = new BCD(65536n);
+
+console.log(n.valueOf());
+console.log(n.get(1));
+console.log(n.get(2));
+console.log(n.get(3));
+console.log(n.get(-10));
+console.log(n.get(-2));
